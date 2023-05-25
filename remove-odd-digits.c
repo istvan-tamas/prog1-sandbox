8119d03b26d50652003f5d12dbb2f1cb70ef6236
istvan-tamas <istvan.tamas90@gmail.com>
2023-05-25 09:51:04 +0200

    Added remove odd digits

---
+++ remove-odd-digits.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *delete_digits(char *original){
+    char *result = (char *) calloc(strlen(original) + 1, sizeof(char));
+    int j = 0;
+    for (int i = 0; i < strlen(original); i++){
+        if (isdigit(original[i])){
+            if(original[i]%2==0){
+                    result[j] = original[i];
+                    j++;
+                }
+        }else{
+            result[j] = original[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+int main(){
+
+    char line[101];
+
+    int n = atoi(gets(line));
+    for(int i=0; i<n; i++){
+        gets(line);
+        char *result = delete_digits(line);
+        puts(result);
+        free(result);
+    }
+
+    return 0;
+}
