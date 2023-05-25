5091def312aaaee60eae86270d8927ccac43a2ae
istvan-tamas <istvan.tamas90@gmail.com>
2023-05-25 09:32:30 +0200

    Fixed palindrome

--- array-palindrome.c
+++ array-palindrome.c
@@ -3,7 +3,7 @@
 
 int is_palindrome(int *numbers, int length)
 {
-    for (int i = 0; i < length / 2; i++)
+    for (int i = 0; i < length; i++)
     {
         if (numbers[i] != numbers[length - 1 - i])
         {
