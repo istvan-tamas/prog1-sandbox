90c0c1ee1aa070f291bd08bc1cf27a08d7edf6bc
Istvan Tamas <istvan.tamas90@gmail.com>
2024-05-13 10:46:29 +0200

    Update airports_n.c

--- new/airports_n.c
+++ new/airports_n.c
@@ -8,76 +8,58 @@ typedef struct {
     int runways;
     int time;
 
-} AIRPORT;
+}AIRPORT;
 
-int cmp(const void *a, const void*b){
-    AIRPORT *left = (AIRPORT *) a;
-    AIRPORT *right = (AIRPORT *) b;
-
-    if(left->runways != right->runways){
-        return -(left->runways - right->runways);
+int cmp(const void *a, const void *b){
+    AIRPORT *ap1 = (AIRPORT*)a;
+    AIRPORT *ap2 = (AIRPORT*)b;
+    if(ap1->runways != ap2->runways){
+        return -(ap2->runways - ap1->runways);
     }
-    if(left->time != right->time){
-        return -(left->time - right->time);
+    if(ap1->time != ap2->time){
+        return -(ap1->time - ap2->time);
     }
-    return strcmp(left->name, right->name);
 
+    return strcmp(ap1->name, ap2->name);
 }
 
-
 int main(int argc, char *argv[]){
-    char line[102];
-    int n = atoi(gets(line));
-    AIRPORT airports[n];
-    
-
     if(argc == 1){
-        fprintf(stderr, "Nincsen input\n");
+        fprintf(stderr, "Nincsen input file megadva!\n");
         return 1;
     }
-    FILE *file = fopen(argv[1], "r");
+
+        FILE *file = fopen(argv[1], "r");  
     if(!file){
-        fprintf(stderr, "Nem lehet megnyitni a fajlt\n");
+        fprintf(stderr, "Nem sikerult megnyitni a fajlt!\n");
         return 2;
     }
-/*
-    while(fgets(line, sizeof(line), file)){
-        line[strlen(line)-1] = '\0';
-        strcpy(airports[length].name, strtok(line, ";"));
-        strcpy(airports[length].city, strtok(NULL, ";"));
-        airports[length].runways = atoi(strtok(NULL, ";"));
-        airports[length].time = atoi(strtok(NULL, ";"));
-        length++;
-    }*/
+
+    char line[101];
+    int n = atoi(fgets(line, sizeof(line), file));
+    AIRPORT ap[n];
 
     for(int i = 0; i < n; i++){
         fgets(line, sizeof(line), file);
         line[strlen(line)-1] = '\0';
-        strcpy(airports[i].name, strtok(line, ";"));
-        strcpy(airports[i].city, strtok(NULL, ";"));
-        airports[i].runways = atoi(strtok(NULL, ";"));
-        airports[i].time = atoi(strtok(NULL, ";"));
+        strcpy(ap[i].name, strtok(line, ";"));
+        strcpy(ap[i].city, strtok(NULL, ";"));
+        ap[i].runways = atoi(strtok(NULL, ";"));
+        ap[i].time = atoi(strtok(NULL, ";"));
     }
-
-    fclose(file);
-    qsort(airports, n, sizeof(AIRPORT), cmp);
     
-if(argc == 2){
-    fprintf(stderr, "Nincsen output\n");
-    return 3;
-}
+    fclose(file);
+    qsort(ap, n, sizeof(AIRPORT), cmp);
 
-file = fopen(argv[2], "w");
-if(!file){
-    fprintf(stderr, "Nem lehet megnyitni a fajlt\n");
-    return 4;
-}
-
-for(int i = 0; i< n; i++){
-    fprintf(file, "%s (%s): %d\n", airports[i].name, airports[i].city, airports[i].time);
-}
-
-fclose(file);
-
+    file = fopen(argv[2], "w");
+    if(!file) {
+        fprintf(stderr, "Nem nyithato meg az output\n");
+        return 4;
+    }
+    
+    for(int i = 0; i < n; i++){
+        fprintf(file, "%s;%s;%d;%d\n", ap[i].name, ap[i].city, ap[i].runways, ap[i].time);
+    }
+    fclose(file);
     return EXIT_SUCCESS;
 }
